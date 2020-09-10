0cfceb911f36ec72ed225396386b555c5f39cbf4
Rita <rita@MacBook-Pro-Anton.local>
2020-09-10 22:09:18 +0300

    add build folder

---
+++ build/precache-manifest.4e57269fe4a46de31ea729106de15147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2537c21db3f16daf1ed78082099a4612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e5d30f0520b5bb8df66",
+    "url": "/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "269a5b776fa9468670ba",
+    "url": "/static/js/2.37ff4382.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37ff4382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5d30f0520b5bb8df66",
+    "url": "/static/js/main.08edcf4b.chunk.js"
+  },
+  {
+    "revision": "4b58567975320dadbf0d",
+    "url": "/static/js/runtime-main.31961638.js"
+  }
+]);
