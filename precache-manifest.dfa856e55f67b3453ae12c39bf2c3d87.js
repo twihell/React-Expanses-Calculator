89a2b4b17f98e5ae4b6ed8d1a4dc114a65f31580
Rita <rita@MacBook-Pro-Anton.local>
2020-09-10 22:32:43 +0300

    Updates

---
+++ precache-manifest.dfa856e55f67b3453ae12c39bf2c3d87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80be6b71cdc23cd4464ecac12b840d5e",
+    "url": "/React-Expanses-Calculator/index.html"
+  },
+  {
+    "revision": "5e5d30f0520b5bb8df66",
+    "url": "/React-Expanses-Calculator/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "269a5b776fa9468670ba",
+    "url": "/React-Expanses-Calculator/static/js/2.37ff4382.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Expanses-Calculator/static/js/2.37ff4382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5d30f0520b5bb8df66",
+    "url": "/React-Expanses-Calculator/static/js/main.08edcf4b.chunk.js"
+  },
+  {
+    "revision": "4408f20f2d2b46ba2216",
+    "url": "/React-Expanses-Calculator/static/js/runtime-main.c2ef9953.js"
+  }
+]);
